2217ff34381734ad11f68b75652b45923a6bfc71
Mick Ho <mick.my.ho@gmail.com>
2019-01-26 23:14:23 +0800

    Add read products continue command

--- continue.py
+++ continue.py
@@ -1,11 +1,19 @@
+import os #operating system
 #讀取檔案
 products = []
-with open('product.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if 'name,price' in line:
-			continue #跳過name price
-		name, price = line.strip().split(',')
-		producets.append([name, price])
+if os.path.isfile('products.csv'): #如果這檔案有無在這個絕對路徑 檢查有無這個檔案
+	print('yes! file exist')
+	with open('product.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if 'name,price' in line:
+				continue #跳過name price
+			name, price = line.strip().split(',')
+			producets.append([name, price])
+else:
+	print('not file exsit')
+
+
+
 
 print(products)
 
